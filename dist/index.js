0a35038b8993eecd99cc6865eb7c9a8285c393aa
mattwwarren <mattwwarren@users.noreply.github.com>
2024-12-03 12:02:45 -0500

    drop warning log to info and update dev dependencies (#404)
    * drop warning log to info and update dev dependencies

--- dist/index.js
+++ dist/index.js
@@ -2823,13 +2823,23 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.configurefetch = void 0;
 exports.api = api;
@@ -2887,7 +2897,7 @@ function inject(response) {
             core.exportVariable(parameterName, effectiveValue);
         }
         else {
-            core.warning(`Ignoring unset value for parameter "${parameterName}" (GitHub Actions does not support unsetting).`);
+            core.info(`Ignoring unset value for parameter "${parameterName}" (GitHub Actions does not support unsetting).`);
         }
     }
 }
@@ -2951,7 +2961,7 @@ async function run() {
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.LIB_VERSION = void 0;
-exports.LIB_VERSION = "3.0.0";
+exports.LIB_VERSION = "3.0.1";
 
 
 /***/ }),
